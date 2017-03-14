9f4be9ae67e9dda43ba45c0a6e911e95bc607b92
ralamos <rodrigoalamos97@gmail.com>
2017-03-14 13:05:17 +0100

    Update Hola.java

--- PruebaGit/src/Git/Hola.java
+++ PruebaGit/src/Git/Hola.java
@@ -2,8 +2,20 @@ package Git;
 
 public class Hola {
 
+	private String nombre;
+	public Hola(String nombre2)
+	{
+		nombre = nombre2;
+	}
+	
+	public void HolaMundo()
+	{
+		System.out.println("Hola desde "+nombre);
+	}
+	
 	public void HolaMundo()
 	{
 		System.out.println("Hola mundo");
 	}
+	
 }
